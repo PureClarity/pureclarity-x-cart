4f1183518c6a6772727847062f40319e2824cfee
Gareth James <gareth.james@pureclarity.com>
2019-12-24 16:27:51 +0000

    Changed OC-X Zones to be on checkoutSuccess target only

--- classes/XLite/Module/PureClarity/Personalisation/View/Zones/Order/Oc02.php
+++ classes/XLite/Module/PureClarity/Personalisation/View/Zones/Order/Oc02.php
@@ -15,5 +15,18 @@ use XLite\Module\PureClarity\Personalisation\View\Zones\AZone;
  */
 class Oc02 extends AZone
 {
+    /** @var string $zoneId */
     protected $zoneId = 'OC-02';
+
+    /**
+     * Adds checkoutSuccess as an allowed target, so it displays on the checkout success page
+     *
+     * @return string[]
+     */
+    public static function getAllowedTargets() : array
+    {
+        $result = parent::getAllowedTargets();
+        $result[] = 'checkoutSuccess';
+        return $result;
+    }
 }
