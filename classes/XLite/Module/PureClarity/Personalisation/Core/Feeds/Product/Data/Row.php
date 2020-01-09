32ce9c1d7549ed5ee8700b8b7e1a6af00a785314
Gareth James <gareth.james@pureclarity.com>
2020-01-09 14:54:29 +0000

    Re-organising code in product feed row data class Adding processing of wholesale prices

--- classes/XLite/Module/PureClarity/Personalisation/Core/Feeds/Product/Data/Row.php
+++ classes/XLite/Module/PureClarity/Personalisation/Core/Feeds/Product/Data/Row.php
@@ -6,11 +6,18 @@
 
 namespace XLite\Module\PureClarity\Personalisation\Core\Feeds\Product\Data;
 
+use XLite;
 use XLite\Base\Singleton;
 use XLite\Core\Database;
+use XLite\Model\AttributeValue\AttributeValueCheckbox;
+use XLite\Model\AttributeValue\AttributeValueHidden;
+use XLite\Model\AttributeValue\AttributeValueSelect;
+use XLite\Model\AttributeValue\AttributeValueText;
 use XLite\Model\Category;
+use XLite\Model\Membership;
 use XLite\Model\Product;
 use XLite\Module\PureClarity\Personalisation\Core\Feeds\FeedRowDataInterface;
+use XLite\Module\XC\ProductVariants\Model\ProductVariant;
 
 /**
  * class Row
@@ -22,6 +29,15 @@ class Row extends Singleton implements FeedRowDataInterface
     /** @var Category[] */
     protected $excludedCats;
 
+    /** @var string */
+    protected $currencyCode;
+
+    /** @var mixed[] */
+    protected $rowData;
+
+    /** @var Membership[] */
+    protected $memberships;
+
     /**
      * Processes the provided Product into an array in the format required for the PureClarity Product Feed
      *
@@ -31,118 +47,53 @@ class Row extends Singleton implements FeedRowDataInterface
      */
     public function getRowData($row) : array
     {
+        $this->rowData = [];
+        
+        if ($this->excludeRow($row)) {
+            return $this->rowData;
+        }
+
+        $this->buildBaseData($row);
+        $this->buildPriceData($row);
+        $this->buildVariantData($row);
+        $this->buildAttributeData($row);
+
+        return $this->rowData;
+    }
+
+    /**
+     * returns whether a row should be excluded from the feed data or not
+     *
+     * @param object|Product $row
+     *
+     * @return bool
+     */
+    protected function excludeRow($row) : bool
+    {
+        $exclude = false;
         if ($row->getPureClarityExcludeFromFeed()) {
-            return [];
-        }
+            $exclude = true;
+        } else {
+            $excludedCats = $this->getExcludedCategories();
 
-        $currencyCode = \XLite::getInstance()->getCurrency()->getCode();
+            if (count($excludedCats) > 0) {
+                $categories = $row->getCategories();
 
-        $categories = $row->getCategories();
+                $categoryIds = [];
+                foreach ($categories as $category) {
+                    $categoryIds[] = $category->getId();
+                }
 
-        $categoryIds = [];
-        foreach ($categories as $category) {
-            $categoryIds[] = $category->getId();
-        }
-
-        $excludedCats = $this->getExcludedCategories();
-        foreach ($excludedCats as $excludedCat) {
-            if (in_array($excludedCat->getId(), $categoryIds)) {
-                // product is in an excluded Category
-                return [];
-            }
-        }
-
-        $resizedURL = '';
-
-        if ($row->getImage()) {
-            list(
-                $usedWidth,
-                $usedHeight,
-                $resizedURL,
-                $retinaResizedURL
-                ) = $row->getImage()->getResizedURL(262, 280);
-        }
-
-        $data = [
-            'Id' => $row->getId(),
-            'Sku' => $row->getSku(),
-            'Title' => $row->getName(),
-            'Description' => [
-                $row->getCommonDescription(),
-                $row->getProcessedBriefDescription()
-            ],
-            'Link' => html_entity_decode($row->getFrontURL()),
-            'Image' => $resizedURL,
-            'Categories' => $categoryIds,
-            'InStock' => $row->isOutOfStock() ? 'false' : 'true',
-            'Prices' => [
-                $row->getPrice() . ' ' . $currencyCode
-            ],
-            'SearchTags' => $row->getPureClaritySearchTags(),
-            'ExcludeFromRecommenders' => $row->getPureClarityExcludeFromRecommenders(),
-            'NewArrival' => $row->getPureClarityNewArrival(),
-            'OnOffer' => $row->getPureClarityOnOffer(),
-        ];
-
-        if ($row->getSalePriceValue() && $row->getSalePriceValue() !== $row->getPrice()) {
-            $data['SalePrices'] = [
-                $row->getSalePriceValue() . ' ' . $currencyCode
-            ];
-        }
-
-        $variants = $row->getVariants();
-        if ($variants && $variants->count() > 0) {
-            $data['AssociatedSkus'] = [];
-            foreach ($variants as $variant) {
-                /** @var \XLite\Module\XC\ProductVariants\Model\ProductVariant $variant */
-                $data['AssociatedSkus'][] = $variant->getSku() ?: $variant->getVariantId();
-                $data['Prices'][] = $variant->getPrice() . ' ' . $currencyCode;
-                if ($variant->getSalePriceValue() && $variant->getSalePriceValue() !== $variant->getPrice()) {
-                    $data['SalePrices'] = [
-                        $variant->getSalePriceValue() . ' ' . $currencyCode
-                    ];
+                foreach ($excludedCats as $excludedCat) {
+                    if (in_array($excludedCat->getId(), $categoryIds)) {
+                        // product is in an excluded Category
+                        $exclude = true;
+                    }
                 }
             }
         }
 
-        if ($row->getPureClarityRecommenderDateRange()) {
-            if ($row->getPureClarityRecommenderStartDate()) {
-                $data['StartDate'] = date('Y-m-d H:i:s', $row->getPureClarityRecommenderStartDate());
-            }
-
-            if ($row->getPureClarityRecommenderEndDate()) {
-                $data['EndDate'] = date('Y-m-d H:i:s', $row->getPureClarityRecommenderEndDate());
-            }
-        }
-
-        $selectAttributes = $row->getAttributeValueS();
-
-        foreach ($selectAttributes as $att) {
-            /** @var $att \XLite\Model\AttributeValue\AttributeValueSelect */
-            $data[str_replace(' ', '_', $att->getAttribute()->getName())] = $att->getAttributeOption()->getName();
-        }
-
-        $hiddenAttributes = $row->getAttributeValueH();
-
-        foreach ($hiddenAttributes as $att) {
-            /** @var $att \XLite\Model\AttributeValue\AttributeValueHidden */
-            $data[str_replace(' ', '_', $att->getAttribute()->getName())] = $att->getAttributeOption()->getName();
-        }
-
-        $checkboxAttributes = $row->getAttributeValueC();
-        foreach ($checkboxAttributes as $att) {
-            /** @var $att \XLite\Model\AttributeValue\AttributeValueCheckbox */
-            $data[str_replace(' ', '_', $att->getAttribute()->getName())] = $att->getValue();
-        }
-
-        $textAttributes = $row->getAttributeValueT();
-
-        foreach ($textAttributes as $att) {
-            $data[str_replace(' ', '_', $att->getAttribute()->getName())] = $att->getValue();
-            /** @var $att \XLite\Model\AttributeValue\AttributeValueText */
-        }
-
-        return $data;
+        return $exclude;
     }
 
     /**
@@ -161,5 +112,216 @@ class Row extends Singleton implements FeedRowDataInterface
         }
 
         return $this->excludedCats;
+    }
+    
+    /**
+     * Adds Base product data to the row data array
+     *
+     * @param object|Product $row
+     *
+     * @return void
+     */
+    protected function buildBaseData($row) : void
+    {
+        $categories = $row->getCategories();
+
+        $categoryIds = [];
+        foreach ($categories as $category) {
+            $categoryIds[] = $category->getId();
+        }
+
+        $this->rowData = [
+            'Id' => $row->getId(),
+            'Sku' => $row->getSku(),
+            'Title' => $row->getName(),
+            'Description' => [
+                $row->getCommonDescription(),
+                $row->getProcessedBriefDescription()
+            ],
+            'Link' => html_entity_decode($row->getFrontURL()),
+            'Image' => $this->getProductImageURL($row),
+            'Categories' => $categoryIds,
+            'InStock' => $row->isOutOfStock() ? 'false' : 'true',
+            'SearchTags' => $row->getPureClaritySearchTags(),
+            'ExcludeFromRecommenders' => $row->getPureClarityExcludeFromRecommenders(),
+            'NewArrival' => $row->getPureClarityNewArrival(),
+            'OnOffer' => $row->getPureClarityOnOffer(),
+        ];
+
+        if ($row->getPureClarityRecommenderDateRange()) {
+            if ($row->getPureClarityRecommenderStartDate()) {
+                $this->rowData['StartDate'] = date('Y-m-d H:i:s', $row->getPureClarityRecommenderStartDate());
+            }
+
+            if ($row->getPureClarityRecommenderEndDate()) {
+                $this->rowData['EndDate'] = date('Y-m-d H:i:s', $row->getPureClarityRecommenderEndDate());
+            }
+        }
+    }
+
+    /**
+     * Gets an image url for the product
+     *
+     * @param object|Product $row
+     *
+     * @return string
+     */
+    protected function getProductImageURL($row) : string
+    {
+        $imageURL = '';
+
+        if ($row->getImage()) {
+            list(
+                $usedWidth,
+                $usedHeight,
+                $imageURL,
+                $retinaResizedURL
+                ) = $row->getImage()->getResizedURL(262, 280);
+        }
+
+        return $imageURL;
+    }
+
+    /**
+     * Adds Price data to the row data array
+     *
+     * @param object|Product $row
+     *
+     * @return void
+     */
+    protected function buildPriceData($row) : void
+    {
+        $currencyCode = $this->getCurrencyCode();
+        $this->rowData['Prices'] = [$row->getPrice() . ' ' . $currencyCode];
+
+        if ($row->getSalePriceValue() && $row->getSalePriceValue() !== $row->getPrice()) {
+            $this->rowData['SalePrices'] = [
+                $row->getSalePriceValue() . ' ' . $currencyCode
+            ];
+        }
+
+        if ($row->isWholesalePricesEnabled()) {
+            $groupPrices = [];
+            foreach ($this->getMemberships() as $membership) {
+                $price = $row->getWholesalePrice($membership);
+                if ($price) {
+                    $this->rowData['GroupPrices'][$membership->getMembershipId()]['Prices'][] = $price;
+                }
+            }
+
+            if (!empty($groupPrices)) {
+                $this->rowData['GroupPrices'] = $groupPrices;
+            }
+        }
+    }
+
+    /**
+     * Adds Variant data to the row data array
+     *
+     * @param object|Product $row
+     *
+     * @return void
+     */
+    protected function buildVariantData($row) : void
+    {
+        $variants = $row->getVariants();
+        if ($variants && $variants->count() > 0) {
+            $currencyCode = $this->getCurrencyCode();
+            $this->rowData['AssociatedSkus'] = [];
+
+            foreach ($variants as $variant) {
+                /** @var ProductVariant $variant */
+                $this->rowData['AssociatedSkus'][] = $variant->getSku() ?: $variant->getVariantId();
+                $this->rowData['Prices'][] = $variant->getPrice() . ' ' . $currencyCode;
+
+                if ($variant->getSalePriceValue() && $variant->getSalePriceValue() !== $variant->getPrice()) {
+                    $this->rowData['SalePrices'] = [
+                        $this->rowData->getSalePriceValue() . ' ' . $currencyCode
+                    ];
+                }
+
+                if ($variant->getProduct()->isWholesalePricesEnabled()) {
+                    foreach ($this->getMemberships() as $membership) {
+                        $price = Database::getRepo(
+                            'XLite\Module\CDev\Wholesale\Model\ProductVariantWholesalePrice'
+                        )->getPrice(
+                            $variant,
+                            $variant->getProduct()->getWholesaleQuantity()
+                                ?: $variant->getProduct()->getMinQuantity($membership),
+                            $membership
+                        );
+
+                        if ($price) {
+                            $this->rowData['GroupPrices'][$membership->getMembershipId()]['Prices'][] = $price;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds Attribute data to the row data array
+     *
+     * @param object|Product $row
+     *
+     * @return void
+     */
+    protected function buildAttributeData($row) : void
+    {
+        $selectAttributes = $row->getAttributeValueS();
+
+        foreach ($selectAttributes as $att) {
+            /** @var $att AttributeValueSelect */
+            $data[str_replace(' ', '_', $att->getAttribute()->getName())] = $att->getAttributeOption()->getName();
+        }
+
+        $hiddenAttributes = $row->getAttributeValueH();
+
+        foreach ($hiddenAttributes as $att) {
+            /** @var $att AttributeValueHidden */
+            $data[str_replace(' ', '_', $att->getAttribute()->getName())] = $att->getAttributeOption()->getName();
+        }
+
+        $checkboxAttributes = $row->getAttributeValueC();
+        foreach ($checkboxAttributes as $att) {
+            /** @var $att AttributeValueCheckbox */
+            $data[str_replace(' ', '_', $att->getAttribute()->getName())] = $att->getValue();
+        }
+
+        $textAttributes = $row->getAttributeValueT();
+
+        foreach ($textAttributes as $att) {
+            $data[str_replace(' ', '_', $att->getAttribute()->getName())] = $att->getValue();
+            /** @var $att AttributeValueText */
+        }
+    }
+
+    /**
+     * Returns an array of all memberships in the store
+     *
+     * @return Membership[]
+     */
+    protected function getMemberships() : array
+    {
+        if ($this->memberships === null) {
+            $this->memberships = Database::getRepo('XLite\Model\Membership')->findAll();
+        }
+        
+        return $this->memberships;
+    }
+
+    /**
+     * Gets the store currency code
+     *
+     * @return string
+     */
+    protected function getCurrencyCode() : string
+    {
+        if ($this->currencyCode === null) {
+            $this->currencyCode = XLite::getInstance()->getCurrency()->getCode();
+        }
+
+        return $this->currencyCode;
     }
 }
