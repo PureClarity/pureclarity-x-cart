aaf1f5cfe3c7b53a47aac4ab26062a0fc3d34703
Gareth James <gareth.james@pureclarity.com>
2021-06-10 17:54:05 +0100

    changed category feed to use new paginated system

--- classes/XLite/Module/PureClarity/Personalization/Core/Feeds/Category/Data/Feed.php
+++ classes/XLite/Module/PureClarity/Personalization/Core/Feeds/Category/Data/Feed.php
@@ -9,6 +9,8 @@ namespace XLite\Module\PureClarity\Personalization\Core\Feeds\Category\Data;
 use XLite\Base\Singleton;
 use XLite\Core\Database;
 use XLite\Model\Category;
+use XLite\Model\QueryBuilder\AQueryBuilder;
+use Doctrine\ORM\QueryBuilder;
 use XLite\Module\PureClarity\Personalization\Core\Feeds\FeedDataInterface;
 
 /**
@@ -19,17 +21,50 @@ use XLite\Module\PureClarity\Personalization\Core\Feeds\FeedDataInterface;
 class Feed extends Singleton implements FeedDataInterface
 {
     /**
-     * Returns all enabled Categories and all categories that are not marked as excluded
+     * Returns all completed orders in the last 12 months
+     *
+     * @return int
+     */
+    public function getFeedCount() : int
+    {
+        return $this->getQuery()->count();
+    }
+
+    /**
+     * Returns all completed orders in the last 12 months
      *
      * @return Category[]
      */
-    public function getFeedData() : array
+    public function getFeedData(int $page, int $pageSize) : array
     {
-        return Database::getRepo('XLite\Model\Category')->findBy(
-            [
-                'enabled' => '1',
-                'pureclarityExcludeFromFeed' => '0'
-            ]
-        );
+        $query = $this->getQuery();
+        $query->setFirstResult(($page - 1) * $pageSize);
+        $query->setMaxResults($pageSize);
+        return $query->getResult();
+    }
+
+    /**
+     * Page cleanup
+     *
+     * Clears query entity manager's object cache.
+     */
+    public function cleanPage() : void
+    {
+        $qb = $this->getQuery();
+        $qb->getQuery()->getEntityManager()->clear();
+    }
+
+    /**
+     * Returns a query for all completed orders in the last 12 months
+     *
+     * @return QueryBuilder|AQueryBuilder
+     */
+    public function getQuery()
+    {
+        return Database::getRepo('XLite\Model\Category')->createQueryBuilder('c')
+            ->andWhere('c.enabled >= :enabled')
+            ->setParameter('enabled', 1)
+            ->andWhere('c.pureclarityExcludeFromFeed = :pureclarityExcludeFromFeed')
+            ->setParameter('pureclarityExcludeFromFeed', 0);
     }
 }
