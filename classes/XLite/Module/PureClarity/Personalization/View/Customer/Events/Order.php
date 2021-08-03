0448f8e9f8ea4ef2870764355fd52f2ecad0ec8d
Gareth James <gareth.james@pureclarity.com>
2021-08-03 14:57:20 +0100

    tweak to order loading so it loads the order differently depending on the parameter used

--- classes/XLite/Module/PureClarity/Personalization/View/Customer/Events/Order.php
+++ classes/XLite/Module/PureClarity/Personalization/View/Customer/Events/Order.php
@@ -61,54 +61,52 @@ class Order extends AView
     protected function getOrderEventData() : string
     {
         $orderData = [];
+        $order = null;
         if (Request::getInstance()->order_id) {
-            $orderId = Request::getInstance()->order_id;
-        } elseif (Request::getInstance()->order_number) {
-            $orderId = Request::getInstance()->order_number;
-        }
-
-        if ($orderId) {
-            /** @var Cart $order */
             $order = Database::getRepo('XLite\Model\Order')
-                ->findOneByOrderNumber($orderId);
+                ->find((int) Request::getInstance()->order_id);
+        } elseif (Request::getInstance()->order_number) {
+            $order = Database::getRepo('XLite\Model\Order')
+                ->findOneByOrderNumber(Request::getInstance()->order_number);
+        }
+        
+        /** @var Cart $order */
+        if ($order) {
+            $address = $order->getProfile()->getBillingAddress();
+            $orderData = [
+                'orderid' => $order->getOrderId(),
+                'firstname' => $address ? $address->getFirstname() : '',
+                'lastname' => $address ? $address->getLastname() : '',
+                'postcode' => $address ? $address->getZipcode() : '',
+                'userid' => $order->getProfile()->getProfileId(),
+                'ordertotal' => $order->getTotal(),
+                'email' => $order->getProfile()->getEmail()
+            ];
 
-            if ($order) {
-                $address = $order->getProfile()->getBillingAddress();
-                $orderData = [
-                    'orderid' => $order->getOrderId(),
-                    'firstname' => $address ? $address->getFirstname() : '',
-                    'lastname' => $address ? $address->getLastname() : '',
-                    'postcode' => $address ? $address->getZipcode() : '',
-                    'userid' => $order->getProfile()->getProfileId(),
-                    'ordertotal' => $order->getTotal(),
-                    'email' => $order->getProfile()->getEmail()
+            $orderItems = [];
+            $items = $order->getItems();
+            foreach ($items as $item) {
+                /** @var OrderItem $item */
+                $orderItems[$item->getItemId()] = [
+                    'id' => $item->getProductId(),
+                    'qty' => $item->getAmount(),
+                    'unitprice' => $item->getPrice(),
+                    'children' => []
                 ];
 
-                $orderItems = [];
-                $items = $order->getItems();
-                foreach ($items as $item) {
-                    /** @var OrderItem $item */
-                    $orderItems[$item->getItemId()] = [
-                        'id' => $item->getProductId(),
-                        'qty' => $item->getAmount(),
-                        'unitprice' => $item->getPrice(),
-                        'children' => []
+                if ($variant = $item->getVariant()) {
+                    $orderItems[$item->getItemId()]['children'] = [
+                        [
+                            'sku' => $variant->getSku() ?: $variant->getVariantId(),
+                            'qty' => $item->getAmount()
+                        ]
                     ];
-
-                    if ($variant = $item->getVariant()) {
-                        $orderItems[$item->getItemId()]['children'] = [
-                            [
-                                'sku' => $variant->getSku() ?: $variant->getVariantId(),
-                                'qty' => $item->getAmount()
-                            ]
-                        ];
-                    }
                 }
-
-                $orderData['items'] = array_values($orderItems);
             }
+
+            $orderData['items'] = array_values($orderItems);
         } else {
-            Logger::logCustom('pureclarity', 'Order tracking error, no order id found', false);
+            Logger::logCustom('pureclarity', 'Order tracking error, no order found', false);
         }
 
         return json_encode($orderData);
